97c2bed908506180f34bccfe29296da6bc8bc1bc
pwf <aaron.lisman@gmail.com>
2023-02-25 13:08:28 -0600

    wip

--- host/src/main.rs
+++ host/src/main.rs
@@ -22,7 +22,15 @@ fn main() {
         match rusb::Context::new() {
             Ok(mut context) => match open_device(&mut context, VID, PID) {
                 Some((mut device, device_desc, mut handle)) => {
-                    let mut buf = [0u8; 63];
+                    match handle.set_auto_detach_kernel_driver(true) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("err configing auto detach: {}", e);
+                            continue;
+                        }
+                    }
+
+                    let mut buf = [0u8; 64];
                     buf[16..32].copy_from_slice(
                         SystemTime::now()
                             .duration_since(UNIX_EPOCH)
@@ -32,32 +40,16 @@ fn main() {
                             .as_slice(),
                     );
                     buf[1] = buf[buf.len() - 1];
-                    match find_writable_endpoint(&mut device, &device_desc, TransferType::Interrupt)
-                    {
-                        Some(endpoint) => {
-                            write_endpoint(&mut handle, endpoint, TransferType::Interrupt, &mut buf)
-                        }
+                    println!("{}", hex::encode(buf));
+                    println!("{}", buf.len());
+                    match find_writable_endpoint(&mut device, &device_desc) {
+                        Some(endpoint) => write_endpoint(&mut handle, endpoint, &mut buf),
                         None => println!("No writable interrupt endpoint"),
                     }
 
-                    match find_writable_endpoint(&mut device, &device_desc, TransferType::Bulk) {
-                        Some(endpoint) => {
-                            write_endpoint(&mut handle, endpoint, TransferType::Bulk, &mut buf)
-                        }
-                        None => println!("No writable bulk endpoint"),
-                    }
-
-                    match find_readable_endpoint(&mut device, &device_desc, TransferType::Interrupt)
-                    {
-                        Some(endpoint) => {
-                            read_endpoint(&mut handle, endpoint, TransferType::Interrupt)
-                        }
+                    match find_readable_endpoint(&mut device, &device_desc) {
+                        Some(endpoint) => read_endpoint(&mut handle, endpoint),
                         None => println!("No readable interrupt endpoint"),
-                    }
-
-                    match find_readable_endpoint(&mut device, &device_desc, TransferType::Bulk) {
-                        Some(endpoint) => read_endpoint(&mut handle, endpoint, TransferType::Bulk),
-                        None => println!("No readable bulk endpoint"),
                     }
                 }
                 None => {}
@@ -100,7 +92,6 @@ fn open_device<T: UsbContext>(
 fn find_writable_endpoint<T: UsbContext>(
     device: &mut Device<T>,
     device_desc: &DeviceDescriptor,
-    transfer_type: TransferType,
 ) -> Option<Endpoint> {
     for n in 0..device_desc.num_configurations() {
         let config_desc = match device.config_descriptor(n) {
@@ -112,7 +103,7 @@ fn find_writable_endpoint<T: UsbContext>(
             for interface_desc in interface.descriptors() {
                 for endpoint_desc in interface_desc.endpoint_descriptors() {
                     if endpoint_desc.direction() == Direction::Out
-                        && endpoint_desc.transfer_type() == transfer_type
+                        && endpoint_desc.transfer_type() == TransferType::Interrupt
                     {
                         return Some(Endpoint {
                             config: config_desc.number(),
@@ -129,14 +120,7 @@ fn find_writable_endpoint<T: UsbContext>(
     None
 }
 
-fn write_endpoint(
-    handle: &mut DeviceHandle<Context>,
-    endpoint: Endpoint,
-    transfer_type: TransferType,
-    buf: &mut [u8],
-) {
-    println!("Writing to endpoint: {:?}", endpoint);
-
+fn write_endpoint(handle: &mut DeviceHandle<Context>, endpoint: Endpoint, buf: &mut [u8]) {
     let has_kernel_driver = match handle.kernel_driver_active(endpoint.iface) {
         Ok(true) => {
             handle.detach_kernel_driver(endpoint.iface).ok();
@@ -151,23 +135,15 @@ fn write_endpoint(
         Ok(_) => {
             let timeout = Duration::from_secs(1);
 
-            match transfer_type {
-                TransferType::Interrupt => {
-                    match handle.write_interrupt(endpoint.address, &buf, timeout) {
-                        Ok(len) => {
-                            println!(" - write: {:?}", len);
-                        }
-                        Err(err) => println!("could not read from endpoint: {}", err),
-                    }
+            match handle.write_interrupt(endpoint.address, &buf, timeout) {
+                Ok(len) => {
+                    println!(" - write: {:?}", len);
                 }
-                TransferType::Bulk => match handle.write_bulk(endpoint.address, &buf, timeout) {
-                    Ok(len) => {
-                        println!(" - write: {:?}", len);
-                    }
-                    Err(err) => println!("could not read from endpoint: {}", err),
-                },
-                _ => (),
+                Err(err) => println!("could not read from endpoint: {}", err),
             }
+            handle
+                .release_interface(endpoint.iface)
+                .unwrap_or_else(|e| println!("err releasing: {}", e));
         }
         Err(err) => println!("could not configure endpoint: {}", err),
     }
@@ -180,7 +156,6 @@ fn write_endpoint(
 fn find_readable_endpoint<T: UsbContext>(
     device: &mut Device<T>,
     device_desc: &DeviceDescriptor,
-    transfer_type: TransferType,
 ) -> Option<Endpoint> {
     for n in 0..device_desc.num_configurations() {
         let config_desc = match device.config_descriptor(n) {
@@ -192,7 +167,7 @@ fn find_readable_endpoint<T: UsbContext>(
             for interface_desc in interface.descriptors() {
                 for endpoint_desc in interface_desc.endpoint_descriptors() {
                     if endpoint_desc.direction() == Direction::In
-                        && endpoint_desc.transfer_type() == transfer_type
+                        && endpoint_desc.transfer_type() == TransferType::Interrupt
                     {
                         return Some(Endpoint {
                             config: config_desc.number(),
@@ -209,11 +184,7 @@ fn find_readable_endpoint<T: UsbContext>(
     None
 }
 
-fn read_endpoint<T: UsbContext>(
-    handle: &mut DeviceHandle<T>,
-    endpoint: Endpoint,
-    transfer_type: TransferType,
-) {
+fn read_endpoint<T: UsbContext>(handle: &mut DeviceHandle<T>, endpoint: Endpoint) {
     println!("Reading from endpoint: {:?}", endpoint);
 
     let has_kernel_driver = match handle.kernel_driver_active(endpoint.iface) {
@@ -228,26 +199,18 @@ fn read_endpoint<T: UsbContext>(
 
     match configure_endpoint(handle, &endpoint) {
         Ok(_) => {
-            let mut buf = [0; 256];
+            let mut buf = [0; 64];
             let timeout = Duration::from_secs(1);
 
-            match transfer_type {
-                TransferType::Interrupt => {
-                    match handle.read_interrupt(endpoint.address, &mut buf, timeout) {
-                        Ok(len) => {
-                            println!(" - read: {:?}", &buf[..len]);
-                        }
-                        Err(err) => println!("could not read from endpoint: {}", err),
-                    }
+            match handle.read_interrupt(endpoint.address, &mut buf, timeout) {
+                Ok(len) => {
+                    println!(" - read: {:?}", &buf[..len]);
                 }
-                TransferType::Bulk => match handle.read_bulk(endpoint.address, &mut buf, timeout) {
-                    Ok(len) => {
-                        println!(" - read: {:?}", &buf[..len]);
-                    }
-                    Err(err) => println!("could not read from endpoint: {}", err),
-                },
-                _ => (),
+                Err(err) => println!("could not read from endpoint: {}", err),
             }
+            handle
+                .release_interface(endpoint.iface)
+                .unwrap_or_else(|e| println!("err releasing: {}", e));
         }
         Err(err) => println!("could not configure endpoint: {}", err),
     }
