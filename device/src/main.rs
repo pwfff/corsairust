8e6d37244446b261d1b95395db38bedfea61f614
pwf <aaron.lisman@gmail.com>
2023-04-10 20:18:05 -0500

    idk

--- device/src/main.rs
+++ device/src/main.rs
@@ -30,7 +30,7 @@ use rp_pico as bsp;
 
 // PIOExt for the split() method that is needed to bring
 // PIO0 into useable form for Ws2812:
-use bsp::hal::pio::{PIOExt, UninitStateMachine, SM0};
+use bsp::hal::pio::{InstalledProgram, PIOExt, UninitStateMachine, SM0};
 
 use bsp::{entry, XOSC_CRYSTAL_FREQ};
 use defmt::*;
@@ -112,25 +112,75 @@ fn main() -> ! {
     let (mut pio1, sm4, sm5, sm6, sm7) = pac.PIO1.split(&mut pac.RESETS);
     let dma = pac.DMA.split(&mut pac.RESETS);
 
+    let program0 = ws2812_pio::init(&mut pio0);
+    let program0_0: InstalledProgram<pac::PIO0>;
+    let program0_1: InstalledProgram<pac::PIO0>;
+    let program0_2: InstalledProgram<pac::PIO0>;
+    unsafe {
+        program0_0 = program0.share();
+        program0_1 = program0.share();
+        program0_2 = program0.share();
+    }
+
     // board is driving 16 LED chains, i guess?
     // we have 8 state machines, so two chains per machine
     const LEDS_PER_DEVICE: usize = 9;
-    let mut leds: LEDs<LEDS_PER_DEVICE, RGB8> = LEDs {
+    let mut leds0: LEDs<LEDS_PER_DEVICE, RGB8> = LEDs {
         channel0: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
         channel1: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
     };
-    let buf0 = buf!(LEDS_PER_DEVICE);
-    let buf1 = buf!(LEDS_PER_DEVICE);
+    let buf0_0 = buf!(LEDS_PER_DEVICE);
+    let buf0_1 = buf!(LEDS_PER_DEVICE);
 
-    let mut light = Ws2812Direct::new(
+    let mut leds1: LEDs<LEDS_PER_DEVICE, RGB8> = LEDs {
+        channel0: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
+        channel1: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
+    };
+    let buf1_0 = buf!(LEDS_PER_DEVICE);
+    let buf1_1 = buf!(LEDS_PER_DEVICE);
+
+    let mut leds2: LEDs<LEDS_PER_DEVICE, RGB8> = LEDs {
+        channel0: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
+        channel1: [RGB8::new(0xFF, 0x00, 0x00); LEDS_PER_DEVICE],
+    };
+    let buf2_0 = buf!(LEDS_PER_DEVICE);
+    let buf2_1 = buf!(LEDS_PER_DEVICE);
+
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut light0 = Ws2812Direct::new(
+        program0_0,
         pins.gpio0.into_mode(),
         pins.gpio1.into_mode(),
-        &mut pio0,
         (dma.ch0, dma.ch1),
-        buf0,
-        buf1,
+        buf0_0,
+        buf0_1,
         sm0,
         clocks.peripheral_clock.freq(),
+        timer.count_down(),
+    );
+
+    let mut light1 = Ws2812Direct::new(
+        program0_1,
+        pins.gpio2.into_mode(),
+        pins.gpio3.into_mode(),
+        (dma.ch2, dma.ch3),
+        buf1_0,
+        buf1_1,
+        sm1,
+        clocks.peripheral_clock.freq(),
+        timer.count_down(),
+    );
+
+    let mut light2 = Ws2812Direct::new(
+        program0_2,
+        pins.gpio4.into_mode(),
+        pins.gpio5.into_mode(),
+        (dma.ch4, dma.ch5),
+        buf2_0,
+        buf2_1,
+        sm2,
+        clocks.peripheral_clock.freq(),
+        timer.count_down(),
     );
 
     let (mut spec_reset, mut spec_strobe, mut spec_out) = (
@@ -188,26 +238,32 @@ fn main() -> ! {
         // Enable the USB interrupt
         pac::NVIC::unmask(pac::Interrupt::USBCTRL_IRQ);
     };
+    let FREQ = clocks.system_clock.freq();
     let core = pac::CorePeripherals::take().unwrap();
     // todo foo lol i'm tired
-    let mut delay = Foo(cortex_m::delay::Delay::new(
-        core.SYST,
-        clocks.system_clock.freq().to_Hz(),
-    ));
+    let mut delay = Foo(cortex_m::delay::Delay::new(core.SYST, FREQ.to_Hz()));
 
     let mut led_pin = pins.led.into_push_pull_output();
 
     info!("up");
 
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    info!("led_us: {}", light0.led_us.to_micros());
+
     let mut poll_count_down = timer.count_down();
     let mut led_count_down = timer.count_down();
     let mut led_on = true;
-    let mut step = 6300;
+    // let mut step = 0;
+    let mut step = 5900;
+    // let mut step = 8000;
+    // let mut step = 12345;
+    // let mut step = 27500;
     // Create a count_down timer for 500 milliseconds
     led_count_down.start(LED_BLINK_RATE_MILLIS.millis());
     poll_count_down.start(USB_POLL_RATE_MILLIS.millis());
+    let mut times_ns: u64 = 0;
+    let mut times_count = 0;
     loop {
+        let start = timer.get_counter();
         if poll_count_down.wait().is_ok() {
             poll_usb();
             poll_count_down.start(USB_POLL_RATE_MILLIS.millis());
@@ -230,80 +286,120 @@ fn main() -> ! {
             // step += 100;
             // info!("{:?}", step);
 
-            info!("{:?}", spectrum.decay);
+            // // gotta fit these back into u32s, so split em up:
+            // // high bits: 0xAABB -> shift, 0x00AA -> truncate, 0xAA
+            // info!("{:X}", (smushed >> 32) as u32);
+            // // low bits: 0xAABB -> truncate, 0xBB
+            // info!("{:X}", smushed as u32);
+
+            // info!("{:?}", spectrum.decay);
+            // spectrum.attenuate();
 
             led_count_down.start(LED_BLINK_RATE_MILLIS.millis());
         }
 
-        for (i, v) in spectrum.decay.iter().enumerate() {
-            leds.channel0[i].r = (*v / (u8::MAX as u16)) as u8;
-            leds.channel0[i].g = (*v / (u8::MAX as u16)) as u8;
-            leds.channel0[i].b = (*v / (u8::MAX as u16)) as u8;
-        }
+        // let r = (spectrum.decay[0..3]
+        //     .iter()
+        //     .fold(0, |a: u32, v| a + *v as u32)
+        //     / (3 * u8::MAX as u32)) as u8;
+        // let g = (spectrum.decay[3..5]
+        //     .iter()
+        //     .fold(0, |a: u32, v| a + *v as u32)
+        //     / (2 * u8::MAX as u32)) as u8;
+        // let b = (spectrum.decay[5..7]
+        //     .iter()
+        //     .fold(0, |a: u32, v| a + *v as u32)
+        //     / (2 * u8::MAX as u32)) as u8;
+
+        // for i in 0..leds0.channel0.len() {
+        //     leds0.channel0[i].r = r;
+        //     leds0.channel0[i].g = g;
+        //     leds0.channel0[i].b = b;
+        // }
 
         // for v in leds.channel0.iter_mut() {
         //     hsv::step_hue(v, step);
         // }
-        // for v in leds.channel1.iter_mut() {
-        //     hsv::step_hue(v, step);
-        // }
-        light = light.write(&mut delay, &leds);
+        for v in leds0.channel0.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        for v in leds0.channel1.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        light0 = light0.write(&leds0);
 
-        spectrum.update(
-            &mut adc,
-            &mut delay.0,
-            &mut spec_reset,
-            &mut spec_strobe,
-            &mut spec_out,
-        );
+        for v in leds1.channel0.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        for v in leds1.channel1.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        light1 = light1.write(&leds1);
+
+        for v in leds2.channel0.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        for v in leds2.channel1.iter_mut() {
+            hsv::step_hue(v, step);
+        }
+        light2 = light2.write(&leds2);
+
+        // spectrum.update(
+        //     &mut adc,
+        //     &mut delay.0,
+        //     &mut spec_reset,
+        //     &mut spec_strobe,
+        //     &mut spec_out,
+        // );
 
         // light = light.pump();
+
+        // match timer.get_counter().checked_duration_since(start) {
+        //     Some(d) => {
+        //         times_count += 1;
+        //         times_ns += d.to_nanos();
+        //         if times_count >= 100 {
+        //             let ns = times_ns / 100;
+        //             info!("loop time: {}ns", ns);
+        //             times_count = 0;
+        //             times_ns = 0;
+        //         }
+        //     }
+        //     None => {
+        //         error!("couldn't do since?")
+        //     }
+        // }
     }
 }
 
 mod audio {
     use cortex_m::delay::Delay;
+    use defmt::info;
     use embedded_hal::adc::{Channel, OneShot};
     use embedded_hal::digital::v2::OutputPin;
     use rp_pico::hal::gpio::PushPull;
     use rp_pico::hal::{adc, gpio};
 
     static SPECTRUM_FACTORS: [u16; 7] = [6, 8, 8, 8, 7, 7, 10];
+    const WINDOW_SIZE: usize = 100;
+    const DECAY_WEIGHT: u32 = 10;
+    const PEAKDECAY: u16 = 1;
     const NOISEFLOOR: u16 = 65;
-    const SPECTRUMSMOOTH: u16 = 100;
-    const AGCSMOOTH: u32 = 2500;
-    const GAINUPPERLIMIT: u16 = 200;
-    const GAINLOWERLIMIT: u16 = 1;
-    const PEAKDECAY: u16 = 20;
 
     pub struct Spectrum {
-        pub audio_avg: u32,
-        pub gain_agc: u16,
-        pub decay: [u8; 7],
-        pub peaks: [u8; 7],
-        history: [[u8; 100]; 7],
-        i_history: usize,
-        mins: [u8; 7],
-        mincounts: [u8; 7],
-        maxs: [u8; 7],
-        maxcounts: [u8; 7],
+        pub decay: [u16; 7],
+        pub peaks: [u16; 7],
+        mins: [u16; 7],
+        maxs: [u16; 7],
     }
-
-    //uint8_t I = A + (uint8_t)(((int16_t)(B-A) * F) >> 8)
 
     impl Default for Spectrum {
         fn default() -> Self {
             Self {
-                audio_avg: 300,
-                gain_agc: 10,
-                decay: [0u8; 7],
-                peaks: [0u8; 7],
-                history: [[0u8; 100]; 7],
-                i_history: 0,
-                mins: [0u8; 7],
-                mincounts: [0u8; 7],
-                maxs: [0u8; 7],
-                maxcounts: [0u8; 7],
+                decay: [0u16; 7],
+                peaks: [0u16; 7],
+                mins: [u16::MAX; 7],
+                maxs: [0u16; 7],
             }
         }
     }
@@ -318,14 +414,11 @@ mod audio {
             data: &mut K,
         ) {
             reset.set_high().unwrap();
-            delay.delay_us(5);
-            reset.set_low().unwrap();
             delay.delay_us(10);
+            reset.set_low().unwrap();
+            delay.delay_us(72);
 
-            // store sum of values for AGC
-            let mut analogsum: u16 = 0;
-
-            let mut new_spectrum = [0u16; 7];
+            let mut raws = [0u16; 7];
 
             // cycle through each MSGEQ7 bin and read the analog values
             for i in 0..7 {
@@ -333,55 +426,73 @@ mod audio {
                 strobe.set_low().unwrap();
                 delay.delay_us(25);
 
-                let temp1: u16 = adc.read(data).unwrap();
-                let temp2: u16 = adc.read(data).unwrap();
-                let temp3: u16 = adc.read(data).unwrap();
+                // let temp1: u16 = adc.read(data).unwrap();
+                // let temp2: u16 = adc.read(data).unwrap();
+                // let temp3: u16 = adc.read(data).unwrap();
 
-                // read the analog value
-                new_spectrum[i] = (temp1 + temp2 + temp3) / 3;
+                // raws[i] = (temp1 + temp2 + temp3) / 3;
+                raws[i] = adc.read(data).unwrap();
+
                 strobe.set_high().unwrap();
-                delay.delay_us(30);
+                delay.delay_us(25);
+            }
 
-                // noise floor filter
-                if new_spectrum[i] < NOISEFLOOR {
-                    new_spectrum[i] = 0;
+            for i in 0..7 {
+                let mut raw = raws[i];
+                if raw < NOISEFLOOR {
+                    raw = 0;
                 } else {
-                    new_spectrum[i] -= NOISEFLOOR;
+                    raw -= NOISEFLOOR;
                 }
 
+                // immediately update mins/maxes if we broke the range
+                self.mins[i] = self.mins[i].min(raw);
+                self.maxs[i] = self.maxs[i].max(raw);
+
+                //uint8_t I = A + (uint8_t)(((int16_t)(B-A) * F) >> 8)
+                // let lerped = self.mins[i]
+                //     + (((self.maxs[i] - self.mins[i]) as u32 * raw as u32) ) as u16;
+                let mut lerped = raw;
+                if self.mins[i] != self.maxs[i] {
+                    lerped = (u16::MAX / (self.maxs[i] - self.mins[i])) * (raw - self.mins[i]);
+                }
+                // if i == 5 {
+                //     info!("min {:?} max {:?}", self.mins[i], self.maxs[i]);
+                //     info!("raw {:?} lerped {:?}", raw, lerped);
+                // }
+
                 // apply correction factor per frequency bin
-                new_spectrum[i] *= SPECTRUM_FACTORS[i];
-                new_spectrum[i] /= 10;
-
-                // prepare average for AGC
-                analogsum += new_spectrum[i];
-
-                // apply current gain value
-                new_spectrum[i] = (new_spectrum[i] * self.gain_agc) / 10;
+                // let corrected = lerped * SPECTRUM_FACTORS[i];
 
                 // process time-averaged values
-                self.decay[i] =
-                    ((self.decay[i] * (SPECTRUMSMOOTH - 1)) + new_spectrum[i]) / SPECTRUMSMOOTH;
+                self.decay[i] = (((self.decay[i] as u32 * (DECAY_WEIGHT - 1)) + lerped as u32)
+                    / DECAY_WEIGHT) as u16;
 
                 // process peak values
                 if self.peaks[i] < self.decay[i] {
                     self.peaks[i] = self.decay[i]
                 };
-                self.peaks[i] = self.peaks[i] / PEAKDECAY;
+                if self.peaks[i] > PEAKDECAY {
+                    self.peaks[i] -= PEAKDECAY;
+                } else {
+                    self.peaks[i] = 0;
+                }
             }
 
-            // Calculate audio levels for automatic gain
-            self.audio_avg =
-                (((AGCSMOOTH - 1) * self.audio_avg) + (analogsum as u32 / 7)) / AGCSMOOTH;
+            // if self.i_history0 == 0 {
+            //     info!("raws {:?}", raws);
+            // }
+        }
 
-            // Calculate gain adjustment factor
-            self.gain_agc = (300 / self.audio_avg) as u16;
-            if self.gain_agc > GAINUPPERLIMIT {
-                self.gain_agc = GAINUPPERLIMIT
-            };
-            if self.gain_agc < GAINLOWERLIMIT {
-                self.gain_agc = GAINLOWERLIMIT
-            };
+        pub fn attenuate(&mut self) {
+            for i in 0..7 {
+                if self.mins[i] < u16::MAX - PEAKDECAY {
+                    self.mins[i] += PEAKDECAY;
+                }
+                if self.maxs[i] > PEAKDECAY {
+                    self.maxs[i] -= PEAKDECAY;
+                }
+            }
         }
     }
 }
@@ -406,7 +517,7 @@ mod hsv {
             new = new - MAX_HUE;
         }
 
-        (c.r, c.g, c.b) = hsv2rgb(new, s, v);
+        (c.r, c.g, c.b) = hsv2rgb(new, s, 128);
     }
 
     // neato: https://www.sciencedirect.com/science/article/abs/pii/S0045790615002827
